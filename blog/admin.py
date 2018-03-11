2da9cdef2e6e14acdb2fe39087eeed11ce53cf7c
zhio <14068166741@qq.com>
2018-03-11 22:43:37 +0800

    Initial commit

---
+++ blog/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import Blog_Type,Blog
+# Register your models here.
+@admin.register(Blog_Type)
+class Blog_TypeAdmin(admin.ModelAdmin):
+    list_display = ('id','type_name')
+@admin.register(Blog)
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ('title','blog_type','author','created_time','last_updated_time')
---
+++ blog/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from django.contrib.auth.models import User
+# Create your models here.
+class Blog_Type(models.Model):#博客类型
+    type_name = models.CharField(max_length=15)
+
+    def __str__(self):
+        return self.type_name
+
+class Blog(models.Model):#博客
+    title = models.CharField(max_length=50)
+    blog_type = models.ForeignKey(Blog_Type,on_delete=models.DO_NOTHING)
+    content = models.TextField()
+    author = models.ForeignKey(User,on_delete=models.DO_NOTHING)
+    created_time = models.DateTimeField(auto_now_add=True)
+    last_updated_time = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return "<Blog:%s>" %self.title
+
+
